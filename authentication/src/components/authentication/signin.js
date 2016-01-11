ebc2bb9c3d83db87ea67ba3dcf20e1b5c92e4578
nerito <neritobjr@gmail.com>
2016-01-11 11:56:52 -0200

    sign in using Parse

--- authentication/src/components/authentication/signin.js
+++ authentication/src/components/authentication/signin.js
@@ -6,24 +6,43 @@ var {
   StyleSheet,
   TextInput
 } = React;
-
+var Parse = require('parse/react-native');
 var Button = require('../common/button');
 //<Button text={'Sign In'} onPress={this.onPress} />
 module.exports = React.createClass({
+  getInitialState: function(){
+    return {
+      username: '',
+      password: '',
+      errorMessage: ''
+    };
+  },
   render: function(){
     return (
       <View style={styles.container}>
-        <Text>Signin</Text>
+        <Text>Sign in</Text>
         <Text style={styles.label}>Username:</Text>
-        <TextInput style={styles.input} />
+        <TextInput
+          style={styles.input}
+          value={this.state.username}
+          onChangeText={(text) => this.setState({username: text})}
+        />
         <Text style={styles.label}>Password:</Text>
-        <TextInput secureTextEntry={true} style={styles.input} />
+        <TextInput secureTextEntry={true}
+          style={styles.input}
+          value={this.state.password}
+          onChangeText={(text) => this.setState({password: text})}
+          />
+        <Text style={styles.label} >{this.state.errorMessage}</Text>
         <Button text={'Sign in'} onPress={this.onPress} />
       </View>
     );
   },
   onPress: function(){
-    //todo
+    Parse.User.logIn(this.state.username, this.state.password, {
+      success: (user) => {console.log(user);},
+      error: (data, error) => {this.setState({ errorMessage: error.message });}
+    });
   }
 })
 
--- authentication/src/main.js
+++ authentication/src/main.js
@@ -5,9 +5,14 @@ var {
   StyleSheet
 } = React;
 
+var Parse = require('parse/react-native');
+
 var Signin = require('./components/authentication/signin');
 
 module.exports = React.createClass({
+  componentWillMount: function(){
+    Parse.initialize("yl1VthELCOZzdmq1SxBzA1C3hcemMSXoigjb6tYS", "SvkSbGPEWLZ7kjufztgmp5iheINjAcdtvcDvC5lM");
+  },
   render: function(){
     return(
       <View style={styles.container}>
