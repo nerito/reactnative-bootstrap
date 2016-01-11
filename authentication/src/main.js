9f726cd270fc91bf5e0d6d6d8ae590ad89b7afe6
nerito <neritobjr@gmail.com>
2016-01-11 14:39:18 -0200

    add navigator component

--- authentication/src/main.js
+++ authentication/src/main.js
@@ -1,13 +1,15 @@
 var React = require('react-native');
 var {
-  View,
-  Text,
-  StyleSheet
+  StyleSheet,
+  Navigator
 } = React;
 
 var Parse = require('parse/react-native');
-
 var Signin = require('./components/authentication/signin');
+
+var ROUTES = {
+  signin: Signin
+}
 
 module.exports = React.createClass({
   componentWillMount: function(){
@@ -15,17 +17,22 @@ module.exports = React.createClass({
   },
   render: function(){
     return(
-      <View style={styles.container}>
-        <Signin />
-      </View>
+      <Navigator
+         style={styles.container}
+         initialRoute={{name:'signin'}}
+         renderScene={this.renderScene}
+         configureScene={ () => { return Navigator.SceneConfigs.FloatFromRight; }}
+       />
     );
+  },
+  renderScene: function (route, navigator){
+    var Component = ROUTES[route.name];
+    return <Component />;
   }
 });
 
 var styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    flex: 1
   }
 });
